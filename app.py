5ed653f343ad1d51a578e062b252e9cb5ca8a002
1503051 <1503051@narlabs.org.tw>
2017-09-14 10:44:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
-
+    #res = makeWebhookResult(req)
+    res ='test response'
+ 
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
